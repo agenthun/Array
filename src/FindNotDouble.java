f1c0795d0b2a898e2d1157f81ace2fcd38bdaa50
agenthun <hun333@126.com>
2015-07-18 20:11:58 +0800

    Day 6

---
+++ src/FindNotDouble.java
@@ -0,0 +1,35 @@
+package com.agenthun;
+
+public class FindNotDouble {
+	public static int findNotDouble(int[] a) {
+		int result = a[0];
+		for (int i = 1; i < a.length; i++) {
+			result ^= a[i];
+		}
+		return result;
+	}
+
+	public static int findOnce(int[] a, int appearTimes) {
+		int[] bitCount = new int[32];
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < bitCount.length; j++) {
+				bitCount[j] += ((a[i] >> j) & 1);
+			}
+		}
+		int appearOne = 0;
+		for (int i = 0; i < bitCount.length; i++) {
+			if (bitCount[i] % appearTimes != 0) {
+				appearOne += (1 << i);
+			}
+		}
+		return appearOne;
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 2, 3, 2, 4, 3, 5, 4, 1 };
+		System.out.println("not double : " + findNotDouble(a1));
+
+		int[] a2 = { 1, 2, 1, 2, 4, 2, 4, 4, 1, 3 };
+		System.out.println("only once : " + findOnce(a2, 3));
+	}
+}
---
+++ src/FindOnlyDup.java
@@ -0,0 +1,41 @@
+package com.agenthun;
+
+public class FindOnlyDup {
+	public static int findOnlyDup(int[] a) {
+		int temp1 = 0;
+		int temp2 = 0;
+		for (int i = 0; i < a.length - 1; i++) {
+			temp1 += i + 1;
+			temp2 += a[i];
+		}
+		temp2 += a[a.length - 1];
+		return (temp2 - temp1);
+	}
+
+	public static int findDup(int[] a) {
+		int result = Integer.MAX_VALUE;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > 0) {
+				a[a[i]] = -a[a[i]];
+			} else {
+				a[-a[i]] = -a[-a[i]];
+			}
+		}
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > 0) {
+				result = i;
+			} else {
+				a[i] = -a[i];
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 2, 1, 3, 4 };
+		System.out.println("only dup : " + findOnlyDup(a1));
+
+		int[] a2 = { 4, 2, 1, 3, 4 };
+		System.out.println("dup : " + findDup(a2));
+	}
+}
