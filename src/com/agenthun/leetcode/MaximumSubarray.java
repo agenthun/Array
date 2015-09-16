fd29583221f2b62ef2512c777b06c70ad4baf53d
agenthun <hun333@126.com>
2015-09-17 07:29:46 +0800

    Day 49 最大子元素和

--- src/com/agenthun/leetcode/MaximumSubarray.java
+++ src/com/agenthun/leetcode/MaximumSubarray.java
@@ -6,11 +6,10 @@ package com.agenthun.leetcode;
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
         int max = nums[0];
-        int[] sum = new int[nums.length];
-        sum[0] = nums[0];
+        int sum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            sum[i] = Math.max(sum[i - 1] + nums[i], nums[i]);
-            max = Math.max(max, sum[i]);
+            sum = Math.max(sum+ nums[i], nums[i]);
+            max = Math.max(max, sum);
         }
         return max;
     }
