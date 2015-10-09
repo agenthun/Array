473c1c9b5fadbbe836076c5631e7e084af157334
agenthun <hun333@126.com>
2015-10-09 20:04:20 +0800

    Day 71  黑默丁格炮台

--- README.md
+++ README.md
@@ -48,6 +48,7 @@
     Apples 分苹果(京东笔试)
     AverageAge 平均年龄(美团笔试)
     IsAppear 判断a是否在b中全部出现(百度笔试)
+    AttackYou 黑默丁格炮台(网易笔试)
     TwoSum 在无序数组中找出两数相加为特定数,返回下标
     ThreeSum 在无序数组中找出3数相加为0的组合,返回组合 /# List - FindSum
     ThreeSumClosest 在无序数组中找出3数相加为最接近数target的组合,返回和 /# List - FindSum
---
+++ src/com/agenthun/leetcode/AttackYou.java
@@ -0,0 +1,35 @@
+package com.agenthun.leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by agenthun on 15/10/9.
+ */
+public class AttackYou {
+    double distance(int x1, int y1, int x2, int y2) {
+        double sum2 = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+        return Math.sqrt(sum2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int length = 9;
+        int[] location = new int[length];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bufferedReader.readLine().split(" ");
+        for (int i = 0; i < length; i++) {
+            location[i] = Integer.parseInt(s[i]);
+        }
+
+        int r = location[0];
+        int attack = 0;
+        double d1 = new AttackYou().distance(location[1], location[2], location[7], location[8]);
+        double d2 = new AttackYou().distance(location[3], location[4], location[7], location[8]);
+        double d3 = new AttackYou().distance(location[5], location[6], location[7], location[8]);
+        if (r >= d1) attack++;
+        if (r >= d2) attack++;
+        if (r >= d3) attack++;
+        System.out.println(attack + "X");
+    }
+}
