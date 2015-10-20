6f57e73e0cfa4ac9379c4b414188c18fb85e93b2
agenthun <hun333@126.com>
2015-10-20 20:43:17 +0800

    Day 82 update 求次方

--- src/com/agenthun/leetcode/PowXN.java
+++ src/com/agenthun/leetcode/PowXN.java
@@ -22,10 +22,36 @@ public class PowXN {
         }
     }
 
+    public double myPowAdv(double x, int n) {
+        if (n < 0) {
+            return 1 / powAdv(x, -n);
+        } else {
+            return powAdv(x, n);
+        }
+    }
+
+    private double powAdv(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if ((n & 0x01) == 0) {
+            return powAdv(x * x, n / 2);
+        } else {
+            return powAdv(x * x, n / 2) * x;
+        }
+    }
+
     public static void main(String[] args) {
-        double x = 2;
-        int n = 3;
+        double x = 100;
+        int n = 100;
         System.out.println("x = " + x + ", n = " + n);
+        long beginTime = System.currentTimeMillis();
         System.out.println("myPow(x, n) = " + new PowXN().myPow(x, n));
+        long endTime = System.currentTimeMillis();
+        System.out.println("time = " + (endTime - beginTime));
+
+        beginTime = System.currentTimeMillis();
+        System.out.println("myPowAdv(x, n) = " + new PowXN().myPowAdv(x, n));
+        endTime = System.currentTimeMillis();
+        System.out.println("time = " + (endTime - beginTime));
     }
 }
