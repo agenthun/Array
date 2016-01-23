f2993cb891127354ea54d6eb7bad08464190bc87
agenthun <hun333@126.com>
2016-01-23 21:35:21 +0800

    Day 162  二叉树的镜像递归

--- src/com/agenthun/nowcoder/Mirror.java
+++ src/com/agenthun/nowcoder/Mirror.java
@@ -4,7 +4,34 @@ package com.agenthun.nowcoder;
  * Created by agenthun on 16/1/22.
  */
 public class Mirror {
-    public void mirror(TreeNode root) {
+    public static void mirror(TreeNode root) {
+        /*        if (root == null) return;
+                TreeNode sroot, droot;
+
+		        sroot = root;
+		        while (sroot != null) {
+
+		        }*/
+
+        //递归
+        if (root == null) return;
+        TreeNode pTemp = root.left;
+        root.left = root.right;
+        root.right = pTemp;
+
+        mirror(root.left);
+        mirror(root.right);
+        return;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(3);
+        node.left.left = new TreeNode(4);
+        node.left.right = new TreeNode(5);
+
+        mirror(node);
 
     }
 }
