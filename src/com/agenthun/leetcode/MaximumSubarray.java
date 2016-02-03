b24d756ecebafc2b15febb70a3de5b51c49f9bbc
agenthun <hun333@126.com>
2016-02-03 23:00:29 +0800

    Day 172 fixed MaximumSubarray

--- src/com/agenthun/leetcode/MaximumSubarray.java
+++ src/com/agenthun/leetcode/MaximumSubarray.java
@@ -5,10 +5,11 @@ package com.agenthun.leetcode;
  */
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
         int max = nums[0];
         int sum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            sum = Math.max(sum+ nums[i], nums[i]);
+            sum = Math.max(sum + nums[i], nums[i]);
             max = Math.max(max, sum);
         }
         return max;
