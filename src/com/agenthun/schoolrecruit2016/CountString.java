1ffcd3c69413bc17083b169b04754175b4d440df
agenthun <hun333@126.com>
2016-03-22 08:17:55 +0800

    Day 214 字符串计数(美团)

--- src/com/agenthun/schoolrecruit2016/CountString.java
+++ src/com/agenthun/schoolrecruit2016/CountString.java
@@ -1,5 +1,7 @@
 package com.agenthun.schoolrecruit2016;
 
+import java.util.Scanner;
+
 /**
  * Created by agenthun on 16/3/18.
  * 求字典序在s1和s2之间的，长度在len1到len2的字符串的个数，结果mod 1000007。
@@ -9,11 +11,35 @@ package com.agenthun.schoolrecruit2016;
  * 输出例子: 56
  */
 public class CountString {
-/*    private int countString(String s1, String s2, int len1, int len2) {
+    private static int countString(String s, int len1, int len2) {
+        int count = 0;
+        int tail = (len2 - s.length()) > 0 ? s.length() - 1 : len2 - 1;
+        if (len2 > s.length()) {
+            count += calsubstring(1, len2 - s.length());
+        }
+        for (int i = tail; i >= 0; i--) {
+            int start = (i + 1 - len1) > 0 ? 0 : len1 - (i + 1);
+            count += ('z' - s.charAt(i)) * calsubstring(start, len2 - i - 1);
+        }
+        return count;
+    }
 
-    }*/
+    private static int calsubstring(int len1, int len2) {
+        int rt = 0;
+        rt = (int) (Math.pow(26, len1) * (Math.pow(26, len2 - len1 + 1) - 1) / 25);
+        return rt;
+    }
 
     public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        while (scaner.hasNext()) {
+            String s1 = scaner.next();
+            String s2 = scaner.next();
+            int len1 = scaner.nextInt();
+            int len2 = scaner.nextInt();
 
+            int count = countString(s1, len1, len2) - countString(s2, len1, len2) - 1;
+            System.out.println(count);
+        }
     }
 }
