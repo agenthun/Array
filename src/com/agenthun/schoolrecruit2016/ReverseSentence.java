1dbf6fa6ae690a788a0deae4769026497a450fff
agenthun <hun333@126.com>
2016-04-20 21:33:36 +0800

    Day 239 字符串变形trans(not finish)

--- src/com/agenthun/schoolrecruit2016/ReverseSentence.java
+++ src/com/agenthun/schoolrecruit2016/ReverseSentence.java
@@ -6,6 +6,21 @@ import java.io.InputStreamReader;
 
 /**
  * Created by agenthun on 16/4/2.
+ * 对于一个给定的字符串，我们需要在线性(也就是O(n))的时间里对它做一些变形。写函数public String trans(String s, int n)
+ * 首先这个字符串中包含着一些空格，就像"Hello World"一样，然后我们要做的是把着个字符串中由空格隔开的单词反序，同时反转每个字符的大小写。
+ * 比如"Hello World"变形后就变成了"wORLD hELLO"。
+ * 输入描述:
+ * 给定一个字符串s以及它的长度n(1≤n≤500)
+ * 输出描述:
+ * 返回变形后的字符串。题目保证给定的字符串均由大小写字母和空格构成。
+ * 输入例子:
+ * "This is a sample",16
+ * 输出例子:"SAMPLE A IS tHIS"
+ * 注意空格
+ * input:" h i"，4
+ * output:"I H "
+ * "h i ",4
+ * " I H"
  */
 public class ReverseSentence {
     public char toUpDown(char x) {
@@ -41,10 +56,10 @@ public class ReverseSentence {
         String res = new String();
         for (String x :
                 words) {
-            res += x + " ";
+            res += (x.equals("") ? " " : x) + " ";
         }
 
-        return res.trim();
+        return res;
     }
 
     private static String reverseSentence(String str) {
@@ -75,10 +90,10 @@ public class ReverseSentence {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         //while (true) {
 //        String s = bufferedReader.readLine();
-        int n = 16;
-        String s = "This is a sample";
+        int n = 4;
+        String s = "h i ";
         //System.out.println("ReverseSentence(s) = " + reverseSentence(s));
-        System.out.println("trans(s,new ) = " + new ReverseSentence().trans(s, n));
+        System.out.println("=" + new ReverseSentence().trans(s, n));
 //        }
     }
 }
