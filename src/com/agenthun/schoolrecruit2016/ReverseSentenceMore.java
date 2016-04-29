09ba7330e965b0f7d2ad16ae44012896f7baaa57
agenthun <hun333@126.com>
2016-04-29 11:30:58 +0800

    Day 244 update今日头条 1

--- src/com/agenthun/schoolrecruit2016/ReverseSentenceMore.java
+++ src/com/agenthun/schoolrecruit2016/ReverseSentenceMore.java
@@ -1,54 +1,49 @@
 package com.agenthun.schoolrecruit2016;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
  * Created by agenthun on 16/4/27.
  */
 public class ReverseSentenceMore {
-    public static void reverse(StringBuilder stringBuilder, int index, int len) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < len; i++) {
-            stack.push(stringBuilder.charAt(index + i));
-        }
-        while (!stack.isEmpty()) {
-            stringBuilder.append(stack.pop());
-        }
+    public static String reverse(String s, int index, int end) {
+        int left = index;
+        int right = end;
+        while (left)
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String inputStr = null;
-        inputStr = bufferedReader.readLine();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputStr = scanner.nextLine();
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int n = Integer.parseInt(scanner.nextLine());
         int[] index = new int[n];
-        int[] len = new int[n];
+        int[] end = new int[n];
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(inputStr);
         for (int i = 0; i < n; i++) {
-            String[] s = bufferedReader.readLine().split(" ");
+            String[] s = scanner.nextLine().split(" ");
             index[i] = Integer.parseInt(s[0]);
-            len[i] = Integer.parseInt(s[1]);
-        }
-        StringBuilder stringBuilder = new StringBuilder(inputStr);
-        for (int i = 0; i < n; i++) {
-            reverse(stringBuilder, index[i], len[i]);
+            end[i] = Integer.parseInt(s[0]) + Integer.parseInt(s[1]) - 1;
+            String ss = reverse(inputStr, index[i], end[i]);
+            stringBuilder.append(ss);
+            inputStr = stringBuilder.toString();
+
         }
         System.out.println(stringBuilder.toString());
 /*        while (bufferedReader) {
             int n = input.nextInt();
             int[] index = new int[n];
-            int[] len = new int[n];
+            int[] end = new int[n];
             for (int i = 0; i < n; i++) {
                 index[i] = input.nextInt();
-                len[i] = input.nextInt();
+                end[i] = input.nextInt();
             }
 
             StringBuilder stringBuilder = new StringBuilder(inputStr);
             for (int i = 0; i < n; i++) {
-                reverse(stringBuilder, index[i], len[i]);
+                reverse(stringBuilder, index[i], end[i]);
             }
 
             // 输出结果
