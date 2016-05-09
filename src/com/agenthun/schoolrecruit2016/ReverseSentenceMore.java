c9b27d3dd0b20a8e441e98a6f35c0b34280a39fd
agenthun <hun333@126.com>
2016-05-09 18:54:06 +0800

    Day 249 fixed多次翻转字符串

--- src/com/agenthun/schoolrecruit2016/ReverseSentenceMore.java
+++ src/com/agenthun/schoolrecruit2016/ReverseSentenceMore.java
@@ -4,20 +4,28 @@ import java.util.Scanner;
 
 /**
  * Created by agenthun on 16/4/27.
- */
+ * 现在有一个字符串，你要对这个字符串进行 n 次操作，每次操作给出两个数字：(p, l) 表示当前字符串中从下标为 p 的字符开始的长度为 l 的一个子串。
+ * 你要将这个子串左右翻转后插在这个子串原来位置的正后方，求最后得到的字符串是什么。字符串的下标是从 0 开始的，你可以从样例中得到更多信息。
+ * 输入描述:
+ * 每组测试用例仅包含一组数据，每组数据第一行为原字符串，长度不超过 10 ，仅包含大小写字符与数字。接下来会有一个数字 n 表示有 n 个操作，再接下来有 n 行，
+ * 每行两个整数，表示每次操作的(p , l)。
+ * 保证输入的操作一定合法，最后得到的字符串长度不超过 1000。
+ * 输出描述:
+ * 输出一个字符串代表最后得到的字符串。
+ * 输入例子:
+ * ab
+ * 2
+ * 0 2
+ * 1 3
+
+ 输出例子:
+ abbaabb
+ * */
 public class ReverseSentenceMore {
-    public static String reverse(String s, int index, int len) {
-        int left = index;
-        int right = len + index - 1;
-        char[] cs = s.toCharArray();
-        while (left < right) {
-            char temp = cs[left];
-            cs[left] = cs[right];
-            cs[right] = temp;
-            left++;
-            right--;
-        }
-        return cs.toString();
+    public static void reverse(StringBuffer s, int index, int len) {
+        StringBuffer temp = new StringBuffer(s.substring(index, index + len));
+        temp.reverse();
+        s.insert(index + len, temp.toString());
     }
 
     public static void main(String[] args) {
@@ -27,16 +35,15 @@ public class ReverseSentenceMore {
         int n = Integer.parseInt(scanner.nextLine());
         int[] index = new int[n];
         int[] len = new int[n];
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(inputStr);
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(inputStr);
         for (int i = 0; i < n; i++) {
             String[] s = scanner.nextLine().split(" ");
             index[i] = Integer.parseInt(s[0]);
             len[i] = Integer.parseInt(s[1]);
 
-            String subStr = inputStr.substring(index[i], len[i]);
-            String reverseSubStr = reverse(subStr, 0, subStr.length());
+            reverse(stringBuffer, index[i], len[i]);
         }
-        System.out.println(stringBuilder.toString());
+        System.out.println(stringBuffer.toString());
     }
 }
