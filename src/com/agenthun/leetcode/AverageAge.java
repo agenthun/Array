04e9925d3f0b821736699c0c521c544b55bf9f13
agenthun <hun333@126.com>
2016-06-08 02:09:43 +0800

    Day 253 update 平均年龄

--- src/com/agenthun/leetcode/AverageAge.java
+++ src/com/agenthun/leetcode/AverageAge.java
@@ -2,6 +2,24 @@ package com.agenthun.leetcode;
 
 /**
  * Created by agenthun on 15/9/23.
+ * 已知某公司总人数为W，平均年龄为Y岁(每年3月末计算，同时每年3月初入职新人)，假设每年离职率为x，x>0&&x<1,每年保持所有员工总数不变进行招聘，新员工平均年龄21岁。
+ * 从今年3月末开始，请实现一个算法，可以计算出第N年后公司员工的平均年龄。(结果向上取整)。
+ * 输入描述:
+ * 输入W Y x N
+ * 输出描述:
+ * 输出第N年后的平均年龄
+ * 输入例子: 168 26 0.4 78
+ * 输出例子: 5
+ * <p>
+ * self 分析:
+ * 初始年和 WY,y0=Y
+ * 1: 走Wx, 剩W(1-x), y1=[W(1-x)*y0+Wx*21]/W
+ * 2: y2=[W(1-x)*y1+Wx*21]/W
+ * <p>
+ * yn=[W(1-x)*yn-1 + Wx*21]/W
+ * Yn=(1-x)Yn-1 + 21x ->
+ * Yn - 21 = (1-x)(Yn-1 - 21)
+ * Yn = (Y0-21)(1-x)^(n-1)+21
  */
 public class AverageAge {
     public static int getNYearsAge(double y, double x, int n) {
@@ -23,12 +41,28 @@ public class AverageAge {
         return res;
     }
 
+    public int getNYearsAgeSelf(double y, double x, int n) {
+        double res = (y - 21) * getPowerSelf(1 - x, n - 1) + 21;
+        return (int) Math.ceil(res);
+    }
+
+    public double getPowerSelf(double a, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return a;
+        if ((n & 1) == 0) {
+            return getPowerSelf(a * a, n / 2);
+        } else {
+            return getPowerSelf(a * a, n / 2) * a;
+        }
+    }
+
     public static void main(String[] args) {
         int w = 1;
-        double y = 24;
+        double y = 21;
         double x = 0.9;
-        int n = 1;
+        int n = 3;
         System.out.println("y = " + y + ", x = " + x + ", n = " + n);
         System.out.println("getNYearsAge(y,x,n) = " + getNYearsAge(y, x, n));
+        System.out.println("getPowerSelf(y,x,n) = " + new AverageAge().getNYearsAgeSelf(y, x, n));
     }
 }
