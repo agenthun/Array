c2cbdbbfc152ccdc7a99fe4d2aee2c0145edc5b9
agenthun <hun333@126.com>
2016-07-22 21:21:57 +0800

    Day 276 review Convert & 删除重复结点

--- src/com/agenthun/nowcoder/Convert.java
+++ src/com/agenthun/nowcoder/Convert.java
@@ -4,6 +4,7 @@ package com.agenthun.nowcoder;
  * Created by agenthun on 16/2/21.
  * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
  * 要求不能创建任何新的结点，只能调整树中结点指针的指向。
+ * 即中序遍历
  */
 public class Convert {
     private TreeNode leftLast = null;
@@ -11,24 +12,24 @@ public class Convert {
     public TreeNode Convert(TreeNode pRootOfTree) {
         if (pRootOfTree == null) return null;
 
-
+        //无子结点
         if (pRootOfTree.left == null && pRootOfTree.right == null) {
             leftLast = pRootOfTree;
             return pRootOfTree;
         }
 
-        TreeNode left = Convert(pRootOfTree.left);
-        if (left != null) {
+        TreeNode left = Convert(pRootOfTree.left); //遍历到左结点,递归
+        if (left != null) { //链:leftLast <-> pRootOfTree
             leftLast.right = pRootOfTree;
             pRootOfTree.left = leftLast;
         }
         leftLast = pRootOfTree;
 
-        TreeNode right = Convert(pRootOfTree.right);
-        if (right != null) {
+        TreeNode right = Convert(pRootOfTree.right);//遍历到右结点,递归
+        if (right != null) {//链:pRootOfTree <-> right
             right.left = pRootOfTree;
             pRootOfTree.right = right;
         }
-        return left != null ? left : pRootOfTree;
+        return left != null ? left : pRootOfTree; //left若为null,则left=pRootOfTree
     }
 }
--- src/com/agenthun/nowcoder/DeleteDuplication.java
+++ src/com/agenthun/nowcoder/DeleteDuplication.java
@@ -2,6 +2,7 @@ package com.agenthun.nowcoder;
 
 /**
  * Created by agenthun on 16/2/18.
+ * 删除重复链表
  */
 public class DeleteDuplication {
     public ListNode deleteDuplication(ListNode pHead) {
@@ -9,17 +10,17 @@ public class DeleteDuplication {
 
         ListNode p0 = new ListNode(0);
         p0.next = pHead;
-        ListNode p1 = p0;
-        while (p1.next != null && p1.next.next != null) {
-            if (p1.next.val == p1.next.next.val) {
-                int dup = p1.next.val;
-                while (p1.next != null && p1.next.val == dup) {
+        ListNode p1 = p0; //p1指向p0,p0后一结点为header
+        while (p1.next != null && p1.next.next != null) { //p1的后一结点与再后者,存在且值相同
+            if (p1.next.val == p1.next.next.val) { //值相同,删除
+                int dup = p1.next.val; //取值
+                while (p1.next != null && p1.next.val == dup) { //遍历p1后结点,值相同,跳两个
                     p1.next = p1.next.next;
                 }
-            } else {
+            } else { //值不同,跳过
                 p1 = p1.next;
             }
         }
-        return p0.next;
+        return p0.next; //返回p0的后一结点,即header
     }
 }
